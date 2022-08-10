43bc8ff1872391e0cecf59c25ce6acb48f8cc056
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-10 14:40:17 -0700

    commit tree 9cd2efe14374d4245d5dc2dac5bf41caa996464d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
 class Triangle
-  # write code here
+  # three arguments
+  attr_reader :a, :b, :c
+
+  # upon initialization
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  # define an instance method
+  def kind
+    # validation method
+    validate_triangle
+
+    # eval type
+    if a == b && b == c
+      # returns a symbol
+      :equilateral
+    elsif a == b || b == c || a == c
+      # returns a symbol
+      :isosceles
+    else
+      # returns a symbol
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    # sides > 0 (0 is not positive apparently)
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    # 2 sides sum > third side
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    # raise custom error if invalid
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  # custom error
+  end
+
 end
